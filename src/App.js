747bed38d189b7cf66e5b1b4ab31e49b3edcc7a9
Евгений Дружняев <druzhniaev@gmail.com>
2020-12-16 22:38:28 +0600

    draw message

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import Board from './components/Board';
 import History from './components/History';
+import StatusMessage from './components/StatusMessage';
 import { calculateWinner } from './helpers';
 
 import './styles/root';
@@ -16,10 +17,6 @@ const App = () => {
 
   const winner = calculateWinner(current.board);
 
-  const message = winner
-    ? `Winner is ${winner}`
-    : `Next player is ${current.isXNext ? 'X' : 'O'}`;
-
   const handleSquareClick = position => {
     if (current.board[position] || winner) {
       return;
@@ -34,24 +31,23 @@ const App = () => {
         return square;
       });
 
-      return prev.concat({ board: newBoard, isXNext: !last.isXNext});
+      return prev.concat({ board: newBoard, isXNext: !last.isXNext });
     });
 
     setCurrentMove(prev => prev + 1);
-
   };
 
-  const moveTo = (move) => {
+  const moveTo = move => {
     setCurrentMove(move);
-  }
+  };
 
   return (
     <div className="app">
       <h1>TIC TAC TOE</h1>
-      <h2>{message}</h2>
+      <StatusMessage winner={winner} current={current}/>
       <Board board={current.board} handleSquareClick={handleSquareClick} />
 
-      <History history={history} moveTo={moveTo} currentMove={currentMove}/> 
+      <History history={history} moveTo={moveTo} currentMove={currentMove} />
     </div>
   );
 };
